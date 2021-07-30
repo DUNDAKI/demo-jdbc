e0a2304df441e6c43d74d1e3c7768b118885cc7e
Dundaki <vigjoaopaulo.com>
2021-07-30 02:28:05 -0300

    reusing instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
@@ -7,10 +8,15 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(3);
+		
+		System.out.println("=== TEST 1: seller findById =====");
+		Seller seller = sellerDao.findById(2);
 		System.out.println(seller);
+		
+		DB.closeConnection();		
 	}
 
 }
